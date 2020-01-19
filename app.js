22b65ed37e8f7a67b13bfb0da986a34f9e319453
HAbd97 <humamabdullah@outlook.com>
2020-01-19 22:28:04 +0530

    Server file edit

--- app.js
+++ app.js
@@ -38,36 +38,36 @@ var books = mongoose.Schema({
 var book = mongoose.model("Book", books);
 
 app.post("/done", urlParser, function(req, res) {
-    upload(req, res, function(err) {
-        if (err) {
-            res.send("File not found");
-        }
-        res.redirect("/addbook");
-        console.log("File Uploaded");
-        var f = req.file.filename;
-        console.log(f);
-
-        var obj = new book({
-            name: req.body.namebk,
-            category: req.body.catbk,
-            author: req.body.authbk,
-            description: req.body.descbk,
-            publishedyear: req.body.pubbk,
-            cost: req.body.cstbk,
-            file: f
-        });
-
-
-        obj.save(function(err, booked) {
+    if (req.body.namebk != null || req.body.filebk != null) {
+        upload(req, res, function(err) {
             if (err) {
-                console.error(err);
+                res.send("File not found");
             }
-            console.log(booked);
+            res.redirect("/addbook");
+            console.log("File Uploaded");
+            var f = req.file.filename;
+            console.log(f);
+
+            var obj = new book({
+                name: req.body.namebk,
+                category: req.body.catbk,
+                author: req.body.authbk,
+                description: req.body.descbk,
+                publishedyear: req.body.pubbk,
+                cost: req.body.cstbk,
+                file: f
+            });
+            obj.save(function(err, booked) {
+                if (err) {
+                    console.error(err);
+                }
+                console.log(booked);
+            });
         });
-
-
-    });
-
+    } else {
+        res.redirect("/add");
+        console.log("Error adding book");
+    }
 })
 app.get("/done", function(req, res) {
     res.send("Not Done");
@@ -128,16 +128,16 @@ app.get('/all', function(req, res) {
     });
 });
 
-app.get("/query/:namebk",function(req,res){
-  book.find({$or:[{name:{$regex:req.params.namebk, $options:'i'}},{ category:{$regex:req.params.namebk, $options:'i'}}]}, function(err, result){
-    if(err){
-      console.log("Error");
-    }
-    let data = {
-        books: result
-    };
-    res.send(data);
-  });
+app.get("/query/:namebk", function(req, res) {
+    book.find({ $or: [{ name: { $regex: req.params.namebk, $options: 'i' } }, { category: { $regex: req.params.namebk, $options: 'i' } }] }, function(err, result) {
+        if (err) {
+            console.log("Error");
+        }
+        let data = {
+            books: result
+        };
+        res.send(data);
+    });
 })
 
 app.get("/free", function(req, res) {
@@ -150,7 +150,7 @@ app.get("/free", function(req, res) {
 })
 
 app.get("/allcat/:catbk", function(req, res) {
-    book.find({ category:{$regex:req.params.catbk, $options:'i'}}, function(err, result) {
+    book.find({ category: { $regex: req.params.catbk, $options: 'i' } }, function(err, result) {
         let data = {
             books: result
         };
@@ -159,7 +159,7 @@ app.get("/allcat/:catbk", function(req, res) {
 })
 
 app.get("/catfic", function(req, res) {
-    book.find({ category:"Fiction"}, function(err, result) {
+    book.find({ category: "Fiction" }, function(err, result) {
         let data = {
             books: result
         };
@@ -167,7 +167,7 @@ app.get("/catfic", function(req, res) {
     });
 })
 app.get("/catnfic", function(req, res) {
-    book.find({ category:"Non-Fiction"}, function(err, result) {
+    book.find({ category: "Non-Fiction" }, function(err, result) {
         let data = {
             books: result
         };
@@ -175,7 +175,7 @@ app.get("/catnfic", function(req, res) {
     });
 })
 app.get("/catbio", function(req, res) {
-    book.find({ category:"Biography"}, function(err, result) {
+    book.find({ category: "Biography" }, function(err, result) {
         let data = {
             books: result
         };
